909a4781e2206845951e656b50cffbbbfcabd01b
Hasan Kemal Erkaraca <hasankemal96mail.com>
2020-12-22 18:22:45 +0300

    initial commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,45 +1,85 @@
 import React from 'react';
 
 class LoginPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {value:''};
-
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      valueOfEmail: '',
+      valueOfPassword: '',
+      userInformation: []
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleChange(event){
-this.setState({value: event.target.value});
+  componentDidMount() {
+      fetch('/*URL*/')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ contacts: data })
+      })
+      .catch(console.log)
   }
 
-  handleSubmit(event){
-    let sendMail = {
-      "email":'dasfda',
-      "password":'dsfaf'
-    }
-    alert('A name was submitted: ' + this.state.value);
+
+  handleChangeEmail(event) {
+    this.setState({ 
+      valueOfEmail: event.target.value
+    });    
+  }
+
+  handleChangePassword(event) {
+    this.setState({ 
+      valueOfPassword: event.target.value
+    });    
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
+
+    let sendMail = {
+      "email": this.state.valueOfEmail,
+      "password": this.state.valueOfPassword
+    }
+
+    PostData(sendMail).then(result => {
+      console.log(result);
+    });
+
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <div className = "LoginPage">
+        <div className="LoginPage">
           <h1>BAZAAR</h1>
-          <input type = "text" id= "email" name= "email" value={this.state.value} onChange={this.handleChange} placeholder="example@example.com"/>
-          <input type = "text" id= "password" name= "password" placeholder="password"/>          
+          <input type="text" id="email" name="email" value={this.state.value} onChange={this.handleChangeEmail} placeholder="example@example.com" />
+          <input type="text" id="password" name="password" value={this.state.value} onChange={this.handleChangePassword} placeholder="password" />
           <button type="submit" value="Submit" id="loginButton">Login</button>
         </div>
-      </form>      
+      </form>
     );
   }
 }
 export default LoginPage;
 
 
-
-
-
-
-
+export function PostData(userData) {
+  let BaseUrl = "https://localhost:8080/api/login";
+  return new Promise((resolve, reject) => {
+  fetch(BaseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(response => response.json())
+    .then(responseJson => {
+      resolve(responseJson);
+    })
+    .catch(error => {
+      reject(error);
+    });
+  })};
